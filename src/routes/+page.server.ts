c19b3b4033aaff574665aedc44d24af82b55bd8b
Sean Lailvaux <142589+seanlail@users.noreply.github.com>
2023-03-12 13:38:48 +0000

    Correctly get the layout change in the client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,12 @@ import type { PageServerLoadEvent } from './$types';
 export async function load(event: PageServerLoadEvent) {
 	console.log(Date.now(), '/+page.server before parent', !!event.locals.user);
 
-	await event.parent();
+	const parentData = await event.parent();
 
 	console.log(Date.now(), '/+page.server after parent', !!event.locals.user);
 
 	return {
+		...parentData,
 		random: Math.round(Math.random() * 9999)
 	};
 }
