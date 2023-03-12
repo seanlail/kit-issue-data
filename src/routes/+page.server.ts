060b17ba9a6fcb3595d459f446f5117d5a441db0
Sean Lailvaux <142589+seanlail@users.noreply.github.com>
2023-03-12 13:31:31 +0000

    Call event.parent

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,12 @@
 import type { PageServerLoadEvent } from './$types';
 
 export async function load(event: PageServerLoadEvent) {
-	console.log(Date.now(), '/+page.server', !!event.locals.user);
+	console.log(Date.now(), '/+page.server before parent', !!event.locals.user);
+
+	await event.parent();
+
+	console.log(Date.now(), '/+page.server after parent', !!event.locals.user);
+
 	return {
 		random: Math.round(Math.random() * 9999)
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	console.log(Date.now(), '/+page render', !!data.user);
+	console.log(Date.now(), '/+page render', !!data.user, !!$page.data.user);
+
+	onMount(() => {
+		console.log(Date.now(), '/+page mount', !!data.user, !!$page.data.user);
+	});
 </script>
 
 <h1>Home</h1>
